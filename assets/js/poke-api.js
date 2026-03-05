a444c3e4960a7012c68377aca22db1ea2fd08c42
Alvaro <alvaroalves928@gmail.com>
2026-03-05 17:32:44 +0000

    adicionado as funcionalidades de buscar pokemons e filtro

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,35 +1,66 @@
+const pokeApi = {};
 
-const pokeApi = {}
+const TYPE_COLORS = {
+  normal:   '#A8A77A',
+  fire:     '#EE8130',
+  water:    '#6390F0',
+  electric: '#F7D02C',
+  grass:    '#7AC74C',
+  ice:      '#96D9D6',
+  fighting: '#C22E28',
+  poison:   '#A33EA1',
+  ground:   '#E2BF65',
+  flying:   '#A98FF3',
+  psychic:  '#F95587',
+  bug:      '#A6B91A',
+  rock:     '#B6A136',
+  ghost:    '#735797',
+  dragon:   '#6F35FC',
+  dark:     '#705746',
+  steel:    '#B7B7CE',
+  fairy:    '#D685AD',
+};
 
-function convertPokeApiDetailToPokemon(pokeDetail) {
-    const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
-
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
-
-    pokemon.types = types
-    pokemon.type = type
-
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
-
-    return pokemon
+function mapPokemonToModel(rawPokemon) {
+  const pokemon = new Pokemon();
+  pokemon.id        = rawPokemon.id;
+  pokemon.name      = rawPokemon.name;
+  pokemon.types     = rawPokemon.types.map(t => t.type.name);
+  pokemon.mainType  = pokemon.types[0];
+  pokemon.photo     =
+    rawPokemon.sprites.other['official-artwork'].front_default ||
+    rawPokemon.sprites.front_default;
+  pokemon.height    = rawPokemon.height;
+  pokemon.weight    = rawPokemon.weight;
+  pokemon.stats     = rawPokemon.stats;
+  pokemon.abilities = rawPokemon.abilities;
+  pokemon.baseExperience = rawPokemon.base_experience;
+  pokemon.speciesUrl = rawPokemon.species.url;
+  return pokemon;
 }
 
-pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
-}
+pokeApi.getPokemonDetail = async (pokemon) => {
+  const res = await fetch(pokemon.url);
+  const data = await res.json();
+  return mapPokemonToModel(data);
+};
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+pokeApi.getPokemons = async (offset = 0, limit = 20) => {
+  const res  = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
+  const data = await res.json();
+  const details = await Promise.all(data.results.map(p => pokeApi.getPokemonDetail(p)));
+  return { results: details, next: data.next };
+};
 
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
-}
+pokeApi.getPokemonSpecies = async (url) => {
+  const res  = await fetch(url);
+  const data = await res.json();
+  const flavorEntry =
+    data.flavor_text_entries.find(e => e.language.name === 'pt') ||
+    data.flavor_text_entries.find(e => e.language.name === 'en');
+  return flavorEntry
+    ? flavorEntry.flavor_text.replace(/\f|\n/g, ' ')
+    : '';
+};
+
+pokeApi.TYPE_COLORS = TYPE_COLORS;
