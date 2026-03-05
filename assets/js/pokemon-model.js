706bc80a8fd9aee173130c87df9175d2519fb6fa
Alvaro <alvaroalves928@gmail.com>
2026-03-05 17:28:40 +0000

    adicionado a funcionalidade de modal no card

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,8 +1,31 @@
-
 class Pokemon {
-    number;
-    name;
-    type;
-    types = [];
-    photo;
+  constructor() {
+    this.id        = null;
+    this.name      = '';
+    this.types     = [];
+    this.mainType  = '';
+    this.photo     = '';
+    this.height    = 0;
+    this.weight    = 0;
+    this.stats     = [];
+    this.abilities = [];
+    this.baseExperience = null;
+    this.speciesUrl = '';
+  }
+
+  get totalPower() {
+    return this.stats.reduce((sum, s) => sum + s.base_stat, 0);
+  }
+
+  get formattedNumber() {
+    return `#${String(this.id).padStart(3, '0')}`;
+  }
+
+  get formattedHeight() {
+    return `${(this.height / 10).toFixed(1)} m`;
+  }
+
+  get formattedWeight() {
+    return `${(this.weight / 10).toFixed(1)} kg`;
+  }
 }
